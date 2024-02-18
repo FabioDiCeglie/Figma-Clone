35525f324536f3b21a1fd6e336cba196edb8afc7
FabioDiCeglie <fabiod@backbase.com>
2024-02-18 12:55:35 +0100

    feat: setting up homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 export default function Page() {
-  return (
-       <h1 className='text-5xl text-white'>Liveblocks Figma Clone</h1>
-  );
+    return (
+        <div className='h-[100vh] w-full flex justify-center items-center text-center'>
+            <h1 className='text-2xl'>Live Figma Clone</h1>
+        </div>
+    );
 }
