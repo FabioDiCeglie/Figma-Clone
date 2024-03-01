1e1f8bb646d2238e8489f7249715f1d027d8fe98
FabioDiCeglie <fabiod@backbase.com>
2024-03-01 04:55:45 +0100

    feat: start working on the live cursor

--- components/cursor/LiveCursor.tsx
+++ components/cursor/LiveCursor.tsx
@@ -1,21 +1,21 @@
-import {LiveCursorProps} from "@/types/type";
-import Cursor from "@/components/cursor/Cursor";
-import {COLORS} from "@/constants";
+import { LiveCursorProps } from '@/types/type';
+import Cursor from '@/components/cursor/Cursor';
+import { COLORS } from '@/constants';
 
-const LiveCursor = ({others}: LiveCursorProps) => {
-    others.map(({connectionId, presence}) => {
-        if (!presence) return null
+const LiveCursors = ({ others }: LiveCursorProps) => {
+  return others.map(({ connectionId, presence }) => {
+    if (!presence) return null;
 
-        return (
-            <Cursor
-                key={connectionId}
-                color={COLORS[Number(connectionId) % COLORS.length]}
-                x={presence.cursor.x}
-                y={presence.cursor.y}
-                message={presence.message}
-            />
-        )
-    })
-}
+    return (
+      <Cursor
+        key={connectionId}
+        color={COLORS[Number(connectionId) % COLORS.length]}
+        x={presence.cursor.x}
+        y={presence.cursor.y}
+        message={presence.message}
+      />
+    );
+  });
+};
 
-export default LiveCursor;
+export default LiveCursors;
