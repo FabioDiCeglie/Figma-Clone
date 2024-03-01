03a437559ee1836ead85897549f8823d418f8d5f
FabioDiCeglie <fabiod@backbase.com>
2024-03-01 05:39:33 +0100

    feat: see the others message live

--- components/cursor/Cursor.tsx
+++ components/cursor/Cursor.tsx
@@ -1,4 +1,4 @@
-import CursorSVG from "@/public/assets/CursorSVG";
+import CursorSVG from '@/public/assets/CursorSVG';
 
 type Props = {
   color: string;
@@ -16,6 +16,16 @@ const Cursor = ({ color, x, y, message }: Props) => {
       <CursorSVG color={color} />
 
       {/* Message */}
+      {message && (
+        <div
+          className='absolute left-2 top-5 rounded-3xl px-4 py-2'
+          style={{ backgroundColor: color }}
+        >
+          <p className='text-white whitespace-nowrap text-sm leading-relaxed'>
+            {message}
+          </p>
+        </div>
+      )}
     </div>
   );
 };
