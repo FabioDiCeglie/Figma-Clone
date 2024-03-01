636506c89127b9cd67c5420836d21d84d81e9c00
FabioDiCeglie <fabiod@backbase.com>
2024-03-01 05:07:38 +0100

    feat: created function handling the live cursors positions

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,7 @@
-"use client"
+'use client';
 
-import Live from "@/components/Live";
+import Live from '@/components/Live';
 
 export default function Page() {
-    return (
-        <div className='h-[100vh] w-full flex justify-center items-center text-center'>
-            <h1 className='text-2xl'>Live Figma Clone</h1>
-
-            <Live />
-        </div>
-    );
+  return <Live />;
 }
--- components/Live.tsx
+++ components/Live.tsx
@@ -1,10 +1,37 @@
 import LiveCursors from '@/components/cursor/LiveCursor';
-import { useOthers } from '@/liveblocks.config';
+import { useMyPresence, useOthers } from '@/liveblocks.config';
+import { useCallback } from 'react';
 
 const Live = () => {
   const others = useOthers();
+  const [{ cursor }, updateMyPresence] = useMyPresence() as any;
+
+  const handlePointerMove = useCallback((event: React.PointerEvent) => {
+    event.preventDefault();
+    const x = event.clientX - event.currentTarget.getBoundingClientRect().x;
+    const y = event.clientY - event.currentTarget.getBoundingClientRect().y;
+    updateMyPresence({ cursor: { x, y } });
+  }, []);
+
+  const handlePointerLeave = useCallback((event: React.PointerEvent) => {
+    event.preventDefault();
+    updateMyPresence({ cursor: null, message: null });
+  }, []);
+
+  const handlePointerDown = useCallback((event: React.PointerEvent) => {
+    const x = event.clientX - event.currentTarget.getBoundingClientRect().x;
+    const y = event.clientY - event.currentTarget.getBoundingClientRect().y;
+    updateMyPresence({ cursor: { x, y } });
+  }, []);
+
   return (
-    <div>
+    <div
+      onPointerMove={handlePointerMove}
+      onPointerLeave={handlePointerLeave}
+      onPointerDown={handlePointerDown}
+      className='h-[100vh] w-full flex justify-center items-center text-center'
+    >
+      <h1 className='text-2xl'>Live Figma Clone</h1>
       <LiveCursors others={others} />
     </div>
   );
