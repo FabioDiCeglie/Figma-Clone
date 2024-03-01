5afa77cb1f40a4e292d3a13aaf8553937ddca93d
FabioDiCeglie <fabiod@backbase.com>
2024-03-01 06:16:37 +0100

    feat: create Reaction Selectors and show it in the page

--- components/Live.tsx
+++ components/Live.tsx
@@ -2,21 +2,27 @@ import LiveCursors from '@/components/cursor/LiveCursor';
 import { useMyPresence, useOthers } from '@/liveblocks.config';
 import { useCallback, useEffect, useState } from 'react';
 import CursorChat from './cursor/CursorChat';
-import { CursorMode } from '@/types/type';
+import { CursorMode, CursorState, Reaction } from '@/types/type';
+import ReactionSelector from './reaction/ReactionSelector';
 
 const Live = () => {
   const others = useOthers();
   const [{ cursor }, updateMyPresence] = useMyPresence() as any;
 
-  const [cursorState, setCursorState] = useState({
+  const [cursorState, setCursorState] = useState<CursorState>({
     mode: CursorMode.Hidden,
   });
 
+  const [reaction, setReaction] = useState<Reaction[]>([]);
+
   const handlePointerMove = useCallback((event: React.PointerEvent) => {
     event.preventDefault();
-    const x = event.clientX - event.currentTarget.getBoundingClientRect().x;
-    const y = event.clientY - event.currentTarget.getBoundingClientRect().y;
-    updateMyPresence({ cursor: { x, y } });
+
+    if (cursor === null || cursorState.mode !== CursorMode.ReactionSelector) {
+      const x = event.clientX - event.currentTarget.getBoundingClientRect().x;
+      const y = event.clientY - event.currentTarget.getBoundingClientRect().y;
+      updateMyPresence({ cursor: { x, y } });
+    }
   }, []);
 
   const handlePointerLeave = useCallback((event: React.PointerEvent) => {
@@ -24,11 +30,38 @@ const Live = () => {
     updateMyPresence({ cursor: null, message: null });
   }, []);
 
-  const handlePointerDown = useCallback((event: React.PointerEvent) => {
-    const x = event.clientX - event.currentTarget.getBoundingClientRect().x;
-    const y = event.clientY - event.currentTarget.getBoundingClientRect().y;
-    updateMyPresence({ cursor: { x, y } });
-  }, []);
+  const handlePointerUp = useCallback(
+    (event: React.PointerEvent) => {
+      setCursorState((state: CursorState) =>
+        cursorState.mode === CursorMode.Reaction
+          ? { ...state, isPress: true }
+          : state
+      );
+    },
+    [cursorState.mode, setCursorState]
+  );
+
+  const handlePointerDown = useCallback(
+    (event: React.PointerEvent) => {
+      const x = event.clientX - event.currentTarget.getBoundingClientRect().x;
+      const y = event.clientY - event.currentTarget.getBoundingClientRect().y;
+      updateMyPresence({ cursor: { x, y } });
+      setCursorState((state: CursorState) =>
+        cursorState.mode === CursorMode.Reaction
+          ? { ...state, isPress: true }
+          : state
+      );
+    },
+    [cursorState.mode, setCursorState]
+  );
+
+  const setReactions = useCallback((reaction: string) => {
+    setCursorState({
+      mode: CursorMode.Reaction,
+      reaction,
+      isPressed: false,
+    });
+  }, [])
 
   useEffect(() => {
     const onKeyUp = (event: KeyboardEvent) => {
@@ -42,6 +75,10 @@ const Live = () => {
         updateMyPresence({ message: '' });
         setCursorState({
           mode: CursorMode.Hidden,
+        });
+      } else if (event.key === 'e') {
+        setCursorState({
+          mode: CursorMode.ReactionSelector,
         });
       }
     };
@@ -65,6 +102,7 @@ const Live = () => {
       onPointerMove={handlePointerMove}
       onPointerLeave={handlePointerLeave}
       onPointerDown={handlePointerDown}
+      onPointerUp={handlePointerUp}
       className='h-[100vh] w-full flex justify-center items-center text-center'
     >
       <h1 className='text-2xl'>Live Figma Clone</h1>
@@ -75,6 +113,12 @@ const Live = () => {
           cursorState={cursorState}
           setCursorState={setCursorState}
           updateMyPresence={updateMyPresence}
+        />
+      )}
+
+      {cursorState.mode === CursorMode.ReactionSelector && (
+        <ReactionSelector
+          setReaction={setReactions}
         />
       )}
 
