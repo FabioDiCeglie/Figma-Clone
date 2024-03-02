3a59e1f8792890762565b9aafdd6a568a0633a85
FabioDiCeglie <fabiod@backbase.com>
2024-03-02 16:25:44 +0100

    chore: useMemo fo users

--- components/users/ActiveUsers.tsx
+++ components/users/ActiveUsers.tsx
@@ -3,32 +3,43 @@ import Avatar from './Avatar';
 
 import styles from './index.module.css';
 import { generateRandomName } from '@/lib/utils';
+import { useMemo } from 'react';
 
 const ActiveUsers = () => {
   const users = useOthers();
   const currentUser = useSelf();
   const hasMoreUsers = users.length > 3;
+  
+  const memoizedUsers = useMemo(() => {
+    return (
+      <div className='flex items-center justify-center gap-1'>
+        <div className='flex pl-3'>
+          {/* Me */}
+          {currentUser && (
+            <Avatar name='You' otherStyles='border-[3px] border-prmary-green' />
+          )}
 
-  return (
-    <main className='flex h-screen w-full select-none place-content-center place-items-center'>
-      <div className='flex pl-3'>
-        {/* Me */}
-        {currentUser && (
-          <Avatar name='You' otherStyles='border-[3px] border-prmary-green' />
-        )}
+          {/* All users */}
+          {users.slice(0, 3).map(({ connectionId }) => {
+            return (
+              <Avatar
+                key={connectionId}
+                name={generateRandomName()}
+                otherStyles='ml-3'
+              />
+            );
+          })}
 
-        {/* All users */}
-        {users.slice(0, 3).map(({ connectionId, info }) => {
-          return (
-            <Avatar key={connectionId} name={generateRandomName()} otherStyles='ml-3' />
-          );
-        })}
-
-        {/* More users */}
-        {hasMoreUsers && <div className={styles.more}>+{users.length - 3}</div>}
+          {/* More users */}
+          {hasMoreUsers && (
+            <div className={styles.more}>+{users.length - 3}</div>
+          )}
+        </div>
       </div>
-    </main>
-  );
+    );
+  }, [users.length]);
+
+  return memoizedUsers;
 };
 
 export default ActiveUsers;
