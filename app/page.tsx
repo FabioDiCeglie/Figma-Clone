90bc034c3490126642b3b7a7db9168b7daad1b00
FabioDiCeglie <fabiod@backbase.com>
2024-03-04 06:49:44 +0100

    feat: created left and right side bar

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 'use client';
 
+import LeftSideBar from '@/components/LeftSideBar';
 import Live from '@/components/Live';
 import NavBar from '@/components/NavBar';
+import RightSideBar from '@/components/RightSideBar';
 
 export default function Page() {
   return (
@@ -9,7 +11,9 @@ export default function Page() {
       <NavBar />
       
       <section className='flex h-full flex-row'>
+        <LeftSideBar />
         <Live />
+        <RightSideBar />
       </section>
     </main>
   );
