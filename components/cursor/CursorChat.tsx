461da16384cd001da7ce53e1e44b77fde05dac71
FabioDiCeglie <fabiod@backbase.com>
2024-03-04 06:43:02 +0100

    chore: adjust cursor chat when press 'e'

--- components/cursor/CursorChat.tsx
+++ components/cursor/CursorChat.tsx
@@ -42,7 +42,9 @@ const CursorChat = ({
         <>
           <CursorSVG color='#0000' />
 
-          <div className='absolute left-2 top-5 bg-blue-500 px-4 py-2 text-sm leading-relaxed text-white rounded-[20px]'>
+          <div className='absolute left-2 top-5 bg-blue-500 px-4 py-2 text-sm leading-relaxed text-white rounded-[20px]'
+               onKeyUp={(event) => event.stopPropagation()}
+          >
             {cursorState.previousMessage && (
               <div>{cursorState.previousMessage}</div>
             )}
