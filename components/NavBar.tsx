3477d69967ff39d7a9f68b90f1f5442d54f41b5d
FabioDiCeglie <fabiod@backbase.com>
2024-03-04 07:09:10 +0100

    feat: starting setting up the NavBar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -2,17 +2,35 @@
 
 import Image from 'next/image';
 import { memo } from 'react';
-import { NavbarProps } from '@/types/type';
+import { ActiveElement, NavbarProps } from '@/types/type';
 import ActiveUsers from './users/ActiveUsers';
+import { navElements } from '@/constants';
+import NewThread from './comments/NewThread';
 
 const NavBar = ({ activeElement }: NavbarProps) => {
   return (
     <nav className='flex select-none items-center justify-between gap-4 bg-primary-black px-5 text-white'>
       <Image src='/assets/logo.svg' alt='Figma logo' width={58} height={20} />
+
+      <ul className='flex flex-row'>
+        {navElements.map((item: ActiveElement | any) => (
+          <li key={item.name}>
+            {Array.isArray(item.value) ? (
+              <ShapesMenu />
+            ) : item?.value === 'comments' ? (
+              <NewThread />
+            ) : (
+              <button></button>
+            )}
+          </li>
+        ))}
+      </ul>
       <ActiveUsers />
     </nav>
   );
 };
 
-export default memo(NavBar, (prevProps, nextProps) => 
-prevProps.activeElement === nextProps.activeElement);
+export default memo(
+  NavBar,
+  (prevProps, nextProps) => prevProps.activeElement === nextProps.activeElement
+);
