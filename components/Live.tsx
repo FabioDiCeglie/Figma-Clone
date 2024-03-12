15ba317dbeb0c863faae63713f3c2b6428a21637
FabioDiCeglie <fabiod@backbase.com>
2024-03-12 19:34:00 +0100

    fix: some styling

--- components/Live.tsx
+++ components/Live.tsx
@@ -157,7 +157,7 @@ const Live = ({ canvasRef }: Props) => {
       onPointerLeave={handlePointerLeave}
       onPointerDown={handlePointerDown}
       onPointerUp={handlePointerUp}
-      className='h-[100vh] w-full flex justify-center items-center text-center'
+      className='relative h-full w-full flex flex-1 justify-center items-center'
     >
       <canvas ref={canvasRef}/>
 
